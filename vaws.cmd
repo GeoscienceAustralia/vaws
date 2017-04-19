d8290fd4293c4b2be0c3b45c94b77de566fd645d
Brett Walker <brett.walker@ga.gov.au>
2017-04-19 14:42:24 +1000

    using local environment

--- vaws.cmd
+++ vaws.cmd
@@ -1,10 +1,24 @@
 @echo off
 
-set VAWS_DIR=c:\dev\vaws\vaws
 
-set PYTHONPATH=%VAWS_DIR%\src;%PYTHONPATH%
+
+setlocal
+
+set VAWS_DIR=%~dp0
+
+set PYTHONPATH=%VAWS_DIR%src;%PYTHONPATH%
+
+
 echo %PYTHONPATH%
 
+
+
 echo "Starting vaws gui"
+ 
+echo %*
+
 python src\gui\main.py
 
+ %*
+
+endlocal
