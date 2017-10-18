cde66141c1f97899992cff368f70a9b57b2639a1
Brett Walker <brett.walker@ga.gov.au>
2017-10-18 17:03:32 +1100

    Add windows specific libraries and build commands

---
+++ build/bld.bat
@@ -0,0 +1,8 @@
+mkdir %SP_DIR%\vaws
+mkdir %SP_DIR%\vaws\gui
+mkdir %SP_DIR%\vaws\model
+xcopy vaws\gui %SP_DIR%\vaws\gui /S /Y
+xcopy vaws\model %SP_DIR%\vaws\model /S /Y
+xcopy vaws\*.py %SP_DIR%\vaws /Y
+mkdir %SP_DIR%\vaws\scenarios 
+xcopy scenarios %SP_DIR%\vaws\scenarios /S /Y
