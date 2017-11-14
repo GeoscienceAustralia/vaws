5f9f3ff3a1b233bfeb47b571952f7fe84b18dd47
Brett Walker <brett.walker@ga.gov.au>
2017-11-14 16:05:27 +1100

    remove the scenarios from the windows conda build

--- build/bld.bat
+++ build/bld.bat
@@ -1,9 +1,7 @@
 mkdir %SP_DIR%\vaws
 mkdir %SP_DIR%\vaws\gui
 mkdir %SP_DIR%\vaws\model
-mkdir %SP_DIR%\vaws\scenarios
 
 xcopy vaws\gui %SP_DIR%\vaws\gui /S /Y
 xcopy vaws\model %SP_DIR%\vaws\model /S /Y
 xcopy vaws\*.py %SP_DIR%\vaws /Y
-xcopy scenarios %SP_DIR%\vaws\scenarios /S /Y
