4e4f1227457b484c774ccc5871010c7c89a8366a
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2018-06-02 11:38:40 +1000

    fix package_name in conda_upload.sh again

--- build/conda_upload.sh
+++ build/conda_upload.sh
@@ -9,5 +9,5 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 PACKAGE_NAME_FULL=$(conda build --output .)
 # conda build .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $PACKAGE_NAME_FULL --force
 #anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PACKAGE_NAME_FULL --force
