6b6ee3896f4395eac48d49d2db977211d41ae6d5
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2018-06-02 15:09:33 +1000

    try with original script

--- build/conda_upload.sh
+++ build/conda_upload.sh
@@ -7,12 +7,12 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
-PACKAGE_NAME_FULL=$(conda build --output .)
-# conda build .
-if [ -e "$PACKAGE_NAME_FULL" ]; then
-    echo "File exists"
-    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $PACKAGE_NAME_FULL --force
-else
-    echo "File does not exist"
-fi
-#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+#PACKAGE_NAME_FULL=$(conda build --output .)
+conda build .
+#if [ -e "$PACKAGE_NAME_FULL" ]; then
+#    echo "File exists"
+#    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $PACKAGE_NAME_FULL --force
+#else
+#    echo "File does not exist"
+#fi
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-py27_1.tar.bz2 --force
