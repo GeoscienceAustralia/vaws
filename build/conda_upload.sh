85f3e3929a2eb9985039e6b924f6558ff7610cd4
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2018-06-02 11:27:05 +1000

    fix package_name in conda_upload.sh

--- build/conda_upload.sh
+++ build/conda_upload.sh
@@ -7,5 +7,7 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
-conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+PACKAGE_NAME_FULL=$(conda build --output .)
+# conda build .
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PACKAGE_NAME_FULL --force
