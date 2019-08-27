55b963f034a13d87fbb9de091c4f10e8598899e7
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-08-27 14:54:59 +1000

    comment remove output directory

--- build/bld.bat
+++ build/bld.bat
@@ -6,5 +6,5 @@ mkdir %SP_DIR%\vaws\scenarios
 xcopy vaws\gui %SP_DIR%\vaws\gui /S /Y
 xcopy vaws\model %SP_DIR%\vaws\model /S /Y
 xcopy vaws\scenarios %SP_DIR%\vaws\scenarios /S /Y
-for /d /r %SP_DIR\vaws %d in (output) do @if exist "%d" rd /s/q "%d"
+REM for /d /r %SP_DIR\vaws %d in (output) do @if exist "%d" rd /s/q "%d"
 xcopy vaws\__init__.py %SP_DIR%\vaws /Y
