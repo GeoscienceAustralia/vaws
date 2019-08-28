8abbfa85099e2fbabdd17c4304b6e71f8feca030
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-08-28 12:38:13 +1000

    still error

--- vaws/gui/build_gui.cmd
+++ vaws/gui/build_gui.cmd
@@ -9,11 +9,11 @@ del %PDIR%\vaws_rc.py
 echo Building UI
 where pyuic5
 where pyrcc5
-REM call pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
+pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
 REM powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py
-REM call pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
+pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
 
 echo Building resources
-REM pyrcc5 %PDIR%\vaws.qrc > %PDIR%\vaws_rc.py
+pyrcc5 %PDIR%\vaws.qrc > %PDIR%\vaws_rc.py
 
 echo done
