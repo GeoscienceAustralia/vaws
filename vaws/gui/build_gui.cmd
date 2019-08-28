18c7091875aab43a48116e2cd347b99a87a09ad9
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-08-28 13:17:10 +1000

    move around buildg_gui

--- vaws/gui/build_gui.cmd
+++ vaws/gui/build_gui.cmd
@@ -8,9 +8,10 @@ del %PDIR%\vaws_rc.py
 
 echo Building UI
 where pyuic5
-call pyuic5.bat
-where pyrcc5
-call pyrcc5.bat
+echo %PATH
+REM call pyuic5.bat
+REM where pyrcc5
+REM call pyrcc5.bat
 REM pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
 REM powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py
 REM pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
