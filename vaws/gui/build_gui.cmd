5d6c10fe8746b71fb945c1eee45be940c23c705c
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-08-29 13:49:43 +1000

    testing ps

--- vaws/gui/build_gui.cmd
+++ vaws/gui/build_gui.cmd
@@ -1,23 +1,17 @@
 @echo off
 echo Building GUI files...
 set PDIR=%~dp0
-echo %PDIR%
+REM echo %PDIR%
 
 del %PDIR%\*_ui.py
 del %PDIR%\vaws_rc.py
 
 echo Building UI
-where pyuic5
-echo %PATH%
-REM call pyuic5.bat
-where pyrcc5  
-type pyrcc5
-REM call pyrcc5.bat
-REM pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
-REM powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py
-REM pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
+pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
+powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py
+pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
 
 echo Building resources
-REM pyrcc5 %PDIR%\vaws.qrc > %PDIR%\vaws_rc.py
+pyrcc5 %PDIR%\vaws.qrc > %PDIR%\vaws_rc.py
 
 echo done
