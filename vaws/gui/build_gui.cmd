17999d96354a87456fd69ea6843c9a50f591b21c
Hyeuk Ryu <hyeuk.ryu@ga.gov.au>
2019-09-02 10:35:24 +1000

    use call when call another batch

--- vaws/gui/build_gui.cmd
+++ vaws/gui/build_gui.cmd
@@ -7,11 +7,9 @@ del %PDIR%\*_ui.py
 del %PDIR%\vaws_rc.py
 
 echo Building UI
-pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
-powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py
-pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
+call pyuic5 --from-imports --output %PDIR%\main_ui.py %PDIR%\ui\main.ui
+powershell -Command "(gc %PDIR%\main_ui.py) -replace 'from matplotlibwidget','from vaws.gui.matplotlibwidget' | Out-File -encoding ASCII %PDIR%\main_ui.py"
+call pyuic5 %PDIR%\ui\house.ui -o %PDIR%\house_ui.py
 
 echo Building resources
-pyrcc5 %PDIR%\vaws.qrc > %PDIR%\vaws_rc.py
-
-echo done
+call pyrcc5 -o %PDIR%\vaws_rc.py %PDIR%\vaws.qrc 
